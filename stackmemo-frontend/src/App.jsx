2d16364a0f5efb100d07ac00687d0a0a082e7ccc
Ganiyat Yakub <yakubganiyat@gmail.com>
2024-08-30 17:15:42 +0100

    code for stacks (Front end update)

--- stackmemo-frontend/src/App.jsx
+++ stackmemo-frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Connect } from '@stacks/connect-react';
-import { StacksMocknet } from '@stacks/network';
+import { StacksTestnet } from '@stacks/network';
 import Header from './components/Header';
 import MessageForm from './components/MessageForm';
 import MessageList from './components/MessageList';
+import StatusMessage from './components/StatusMessage';
+import './App.css';
 
-const network = new StacksMocknet();
+const network = new StacksTestnet();
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const appConfig = {
     appName: 'StackMemo',
     network,
@@ -17,9 +22,10 @@ function App() {
     <Connect authOptions={appConfig}>
       <div className="App">
         <Header />
+        <StatusMessage isLoading={isLoading} error={error} />
         <main>
-          <MessageForm />
-          <MessageList />
+          <MessageForm setIsLoading={setIsLoading} setError={setError} />
+          <MessageList setIsLoading={setIsLoading} setError={setError} />
         </main>
       </div>
     </Connect>
